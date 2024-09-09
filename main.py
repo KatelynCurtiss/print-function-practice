b90338df6427d29721c9dc8c76845d644043e525
Katelyn Curtiss <153852200+KatelynCurtiss@users.noreply.github.com>
2024-09-09 10:57:38 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,16 @@
-print('Hello, Katelyn!')
+print('Good morning!')
 print('Welcome to Career Tech!')
+
+
+# Output
+
+greeting1 = 'Good morning!'
+greeting2 = 'Good afternoon!'
+print(greeting1)
+# Adding two lines between the first and second greetings
+print( )
+print( )
+print(greeting2)
+
+
+
